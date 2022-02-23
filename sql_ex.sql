0e76daf498c0332af5bf45e7d923aa7a75403565
Ruzanova Olga <92842400+OlgaRuzanova@users.noreply.github.com>
2022-02-23 17:23:14 +0300

    Update sql_ex.sql

--- sql_ex.sql
+++ sql_ex.sql
@@ -18,3 +18,33 @@
  SELECT model, speed, hd FROM PC
  WHERE (cd = '12x' OR cd = '24x') AND price < 600;
 
+  -- 6. Для каждого производителя, выпускающего ПК-блокноты c объёмом жесткого диска не менее 10 Гбайт, найти скорости таких ПК-блокнотов. Вывод: производитель, скорость.
+ SELECT DISTINCT Product.maker, Laptop.speed FROM Product INNER JOIN Laptop
+ ON Product.model = Laptop.model 
+ WHERE Laptop.hd >=10;
+ 
+  -- 7. Найдите номера моделей и цены всех имеющихся в продаже продуктов (любого типа) производителя B (латинская буква).
+ SELECT Product.model, PC.price FROM Product INNER JOIN PC ON Product.model = PC.model
+ WHERE maker = 'B'
+ UNION
+ SELECT Product.model, Laptop.price FROM Product INNER JOIN Laptop
+ ON Product.model = Laptop.model
+ WHERE maker ='B'
+ UNION
+ SELECT Product.model, Printer.price FROM Product INNER JOIN Printer
+ ON Product.model = Printer.model
+ WHERE maker ='B';
+ 
+  -- 8. Найдите производителя, выпускающего ПК, но не ПК-блокноты.
+ SELECT maker FROM Product
+ WHERE type = 'PC'
+ EXCEPT 
+ SELECT maker FROM Product
+ WHERE type = 'Laptop';
+ 
+  -- 9. Найдите производителей ПК с процессором не менее 450 Мгц. Вывести: Maker
+ SELECT DISTINCT Product.maker FROM Product
+ INNER JOIN PC
+ ON Product.model = PC.model 
+ WHERE PC.speed >=450;
+
