3f306776cbff0cb5a80ba4c59d7b78d8ace4e9a2
Ruzanova Olga <92842400+OlgaRuzanova@users.noreply.github.com>
2022-06-18 17:23:16 +0300

    Update sql_ex.sql

--- sql_ex.sql
+++ sql_ex.sql
@@ -77,8 +77,14 @@
  SELECT hd
  FROM PC
  GROUP BY hd
- HAVING COUNT(model)>=2
+ HAVING COUNT(model)>=2;
  
+  -- 16. Найдите пары моделей PC, имеющих одинаковые скорость и RAM. В результате каждая пара указывается только один раз, т.е. (i,j), но не (j,i), 
+  --     Порядок вывода: модель с большим номером, модель с меньшим номером, скорость и RAM.
+ SELECT DISTINCT PC_1.model, PC_2.model, PC_1.speed, PC_1.ram
+ FROM PC PC_1 JOIN PC PC_2
+ ON PC_1.speed = PC_2.speed AND PC_1.ram = PC_2.ram 
+ AND PC_1.model > PC_2.model;
  
 
 
